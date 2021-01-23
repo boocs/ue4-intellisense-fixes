1ec23528c55dbd1b6a81c70ac68749beaff42bad
boocs <62588629+boocs@users.noreply.github.com>
2021-01-23 08:52:08 -0700

    Preparing for 0.9.5

--- out/text.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEXT_OK = exports.TEXT_MESSAGE_BUILD_SUCCESSFUL_PROJECT = void 0;
-exports.TEXT_MESSAGE_BUILD_SUCCESSFUL_PROJECT = " You must build your project successfully for a fix to work correctly.\n Restart VSCode after a successful build.";
-exports.TEXT_OK = "OK";
-//# sourceMappingURL=text.js.map
--- src/text.ts
+++
@@ -1,3 +0,0 @@
-
-export const TEXT_MESSAGE_BUILD_SUCCESSFUL_PROJECT = " You must build your project successfully for a fix to work correctly.\n Restart VSCode after a successful build.";
-export const TEXT_OK = "OK";
